f02bed1ce4a0a6fd29f0233b09d6a14b24e4eabe
Bowen Fu <missing>
2021-07-31 22:03:49 +0800

    Add stream samples.

--- stream.cpp
+++ stream.cpp
@@ -9,29 +9,6 @@ struct Stream
     std::function<Stream<ValueT>()> next; // can wrap this as memo-func, + std::optional<ValueT> cached
 };
 
-template <typename T>
-auto ones() -> Stream<T>
-{
-    return {1, ones<T>};
-}
-
-template <typename T>
-auto addStreams(Stream<T> const& s1, Stream<T> const& s2) -> Stream<T>
-{
-    assert(static_cast<bool>(s1.value) == static_cast<bool>(s2.value));
-    if (!s1.value)
-    {
-        return s1;
-    }
-    return {*s1.value + *s2.value, [=]{ return addStreams(s1.next(), s2.next()); }};
-}
-
-template <typename T>
-auto naturals() -> Stream<T>
-{
-    return {0, [=]{ return addStreams(ones<T>(), naturals<T>()); }};
-}
-
 template <typename FuncT, typename ValueT>
 auto streamMap(FuncT&& func, Stream<ValueT> const& stream)
 {
@@ -41,7 +18,7 @@ auto streamMap(FuncT&& func, Stream<ValueT> const& stream)
     {
         return ST{};
     }
-    return ST{func(*stream.value), [=] { return streamMap(std::forward<FuncT>(func), stream.next()); }};
+    return ST{func(*stream.value), [=] { return streamMap(func, stream.next()); }};
 }
 
 template <typename FuncT, typename ValueT>
@@ -81,11 +58,145 @@ auto streamFilter(FuncT&& func, Stream<ValueT> const& stream)
     return lambda();
 }
 
+template <typename IndexT, typename ValueT>
+auto streamRef(IndexT i, Stream<ValueT> const& stream)
+{
+    assert(i >= 0);
+    assert(stream.value);
+    return i == 0 ? *stream.value : streamRef(i - 1, stream.next());
+}
+
+template <typename T>
+auto streamEnumerateInterval(T start, T end)
+{
+    if (start == end)
+    {
+        return Stream<T>{};
+    }
+    return Stream<T>{start, [=]{ return streamEnumerateInterval(start + 1, end); }};
+}
+
+template <typename T>
+auto integersStartingFrom(T start) -> Stream<T>
+{
+    return {start, [=]{ return integersStartingFrom(start + 1); }};
+}
+
+template <typename T>
+auto integers()
+{
+    return integersStartingFrom(T{1});
+}
+
+template <typename T>
+constexpr auto fibgen(T a, T b) -> Stream<T>
+{
+    return {a, [=]{ return fibgen(b, a + b); }};
+}
+
+template <typename T>
+constexpr auto fibs()
+{
+    return fibgen<T>(0, 1);
+}
+
+template <typename T>
+constexpr auto sieve(Stream<T> const& stream) -> Stream<T>
+{
+    auto const& value = *stream.value;
+    auto const& pred = [=](auto&& v) { return v%value != 0; };
+    return Stream<T>{value, [=]
+                     { return sieve(streamFilter(pred, stream.next())); }};
+}
+
+template <typename T>
+constexpr auto primes()
+{
+    return sieve<T>(integersStartingFrom(T{2}));
+}
+
+template <typename T>
+auto addStreams(Stream<T> const& s1, Stream<T> const& s2) -> Stream<T>
+{
+    assert(static_cast<bool>(s1.value) == static_cast<bool>(s2.value));
+    if (!s1.value)
+    {
+        return s1;
+    }
+    return {*s1.value + *s2.value, [=]{ return addStreams(s1.next(), s2.next()); }};
+}
+
+template <typename T>
+auto mulStreams(Stream<T> const& s1, Stream<T> const& s2) -> Stream<T>
+{
+    assert(static_cast<bool>(s1.value) == static_cast<bool>(s2.value));
+    if (!s1.value)
+    {
+        return s1;
+    }
+    return {*s1.value * *s2.value, [=]{ return mulStreams(s1.next(), s2.next()); }};
+}
+
+template <typename T>
+auto ones() -> Stream<T>
+{
+    return {1, ones<T>};
+}
+template <typename T>
+auto integers2() -> Stream<T>
+{
+    return {1, [=]{ return addStreams(ones<T>(), integers2<T>()); }};
+}
+
+template <typename T>
+constexpr auto fibs2() -> Stream<T>
+{
+    return {T{0}, [=]
+            { return Stream<T>{T{1}, [=]
+                               { return addStreams(fibs2<T>(), fibs2<T>().next()); }}; }};
+}
+
+template <typename T>
+constexpr auto scaleStream(Stream<T> const& stream, T factor)
+{
+    return streamMap([=](T v) { return v * factor; }, stream);
+}
+
+template <typename T>
+constexpr auto double_() -> Stream<T>
+{
+    return Stream<T>{1, []{ return scaleStream(double_<T>(), T{2}); }};
+}
+
+template <typename T>
+constexpr auto double2() -> Stream<T>
+{
+    return Stream<T>{T{1}, []{ return addStreams(double_<T>(), double_<T>()); }};
+}
+
+template <typename T>
+constexpr auto factorials() -> Stream<T>
+{
+    return Stream<T>{T{1}, []{ return mulStreams(factorials<T>(), integers<T>().next()); }};
+}
+
+template <typename T>
+constexpr auto partialSum() -> Stream<T>
+{
+    return Stream<T>{T{1}, []{ return addStreams(partialSum<T>(), integers<T>().next()); }};
+}
 
 int32_t main()
 {
     // printStream(ones<int32_t>());
-    auto const even = [](auto&& n) { return n%2 == 0;};
-    printStream(streamFilter(even, naturals<int32_t>()));
+    // auto const even = [](auto&& n) { return n%2 == 0;};
+    // printStream(streamFilter(even, streamEnumerateInterval(100000, 200000)));
+    // printStream(fibs<int64_t>());
+    // printStream(primes<int64_t>());
+    // printStream(fibs2<int64_t>());
+    // printStream(double_<int64_t>());
+    // printStream(double2<int64_t>());
+    // printStream(factorials<int64_t>());
+    printStream(partialSum<int64_t>());
     return 0;
 }
